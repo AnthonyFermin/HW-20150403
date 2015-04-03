aded26f67f243c9833f7b870b68c074d005efafb
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-04-03 18:27:41 -0400

    Added comments to web sanitizer

--- src/nyc/c4q/AnthonyFermin/WebPageSanitizer.java
+++ src/nyc/c4q/AnthonyFermin/WebPageSanitizer.java
@@ -4,7 +4,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
+/** Anthony Fermin
+ * HW due 4/3 2015
+ * Exercise 2
+ * Web Page Sanitizer
  * Created by c4q-anthonyf on 4/1/15.
  */
 public class WebPageSanitizer {
@@ -27,15 +30,26 @@ public class WebPageSanitizer {
         for(int i = 0; i < lines.size(); i++){
             String currentLine = lines.get(i);
 
-            //
+            // checks to see if current line of HTML contains the opening script tag
             if(currentLine.contains("<script") && !scriptLine){
+
+                // gets index of first script open tag
                 int scriptLoc = currentLine.indexOf("<script");
+
+                // adds all text up to the open script tag to sanitizedHTML
                 for(int j = 0; j < scriptLoc; j++){
                     sanitizedHTML += currentLine.charAt(j);
                 }
+
+                // gets text contained after open script tag
                 String afterScript = currentLine.substring(scriptLoc);
+
+                // if text after open tag also contains closing script tag, adds the text after script tag, if any
+                // else, it sets scriptLine boolean to true, letting the program know that it's now search for a closing script tag as a priority
                 if(afterScript.contains("</script>")){
+
                     int endScriptLoc = afterScript.indexOf("</script>");
+
                     if(!(endScriptLoc + 8 >= currentLine.length() - 1)){
                         for(int k = endScriptLoc + 1; k < currentLine.length(); k++){
                             sanitizedHTML += currentLine.charAt(k);
@@ -45,13 +59,22 @@ public class WebPageSanitizer {
                     scriptLine = true;
                 }
 
+             // adds entire currentLine if it is currently not searching for closing script tag(scriptLine = false)
+             // and there is no open script tag in the line
             }else if ((!currentLine.contains("<script")) && !scriptLine){
                 sanitizedHTML += currentLine;
                 sanitizedHTML += "\n";
+
+             // skips the currentLine if currently searching for a closing script tag and currentLine does not contain it
             }else if (scriptLine && !currentLine.contains("</script>")){
                 continue;
+
+             // adds text after closing script tag to sanitizedHTML String if currentLine has a closing tag
+             // and program is currently searching for a closing script tag (scriptLine = true)
+             //  - sets scriptLine to false once done
             }else if(scriptLine && currentLine.contains("</script>")){
                 int endScriptLoc = currentLine.indexOf("</script>");
+
                 if(!(endScriptLoc + 8 >= currentLine.length() - 1)){
                     for(int k = endScriptLoc + 8; k < currentLine.length(); k++){
                         sanitizedHTML += currentLine.charAt(k);
@@ -71,6 +94,9 @@ public class WebPageSanitizer {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a webpage to sanitize: ");
+
         String input = scanner.nextLine();
         URL url = HTTP.stringToURL(input);
         String document = HTTP.get(url); // http://docs.oracle.com/javase/7/docs/api/
